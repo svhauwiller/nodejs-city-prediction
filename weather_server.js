20ef14957b0fae7d1d64c569a172d07200ac1bf0
whauwiller <ec2-user@ip-172-31-29-35.us-west-2.compute.internal>
2015-03-12 15:39:58 +0000

    MongoDB Comment Page

--- weather_server.js
+++ weather_server.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var http = require('http');
 var url = require('url');
+var MongoClient = require('mongodb').MongoClient;
 var root_dir = "html/";
 
 http.createServer( function(req, res) {
@@ -23,6 +24,46 @@ http.createServer( function(req, res) {
 			res.writeHead(200);
 			res.end(JSON.stringify(returnValue));
 		});
+	} else if(urlObj.pathname.indexOf("comment") != -1){
+		if(req.method === "POST"){
+			var jsonData = "";
+			req.on('data', function(chunk){
+				jsonData += chunk;
+			});
+			req.on('end', function(){
+				var reqObj = JSON.parse(jsonData);
+				console.log(reqObj);
+				MongoClient.connect("mongodb://localhost/weather", function(err, db){
+					if(err){
+						throw err;
+					}
+					db.collection('comments').insert(reqObj, function(err, records){
+						console.log("");
+					});	
+				});
+			});
+			res.writeHead(200);
+			res.end("");
+		} else if (req.method === "GET") {
+			var returnValue = "";
+			MongoClient.connect("mongodb://localhost/weather", function(err, db){
+				if(err){
+					throw err;
+				}
+				db.collection('comments', function(err, comments){
+					if(err){
+						throw err;
+					}
+					comments.find(function(err, items){
+						items.toArray(function(err, itemArr){
+							returnValue = JSON.stringify(itemArr);
+							res.writeHead(200);
+							res.end(returnValue);
+						});
+					});
+				});		
+			});
+		}
 	} else {
 		fs.readFile(root_dir + urlObj.pathname, function(err, data){
 			if(err){
