9a1abcb63af291c7ed7b4e6cb0a33e17549b9e59
whauwiller <ec2-user@ip-172-31-29-35.us-west-2.compute.internal>
2015-03-17 17:33:24 +0000

    Added support for Express

--- weather_server.js
+++ weather_server.js
@@ -1,78 +1,87 @@
 var fs = require('fs');
 var http = require('http');
+var https = require('https');
 var url = require('url');
+var express = require('express');
+var bodyParser = require('body-parser');
+var basicAuth = require('basic-auth-connect');
 var MongoClient = require('mongodb').MongoClient;
-var root_dir = "html/";
 
-http.createServer( function(req, res) {
-	var urlObj = url.parse(req.url, true, false);
-	if(urlObj.pathname.indexOf("getcity") != -1){
-		fs.readFile('cities.dat.txt', function(err, data) {
+var root_dir = "html/";
+var app = express();
+
+var options = {
+	host: '127.0.0.1',
+	key: fs.readFileSync('ssl/server.key'),
+	cert: fs.readFileSync('ssl/server.crt')
+};
+
+var auth = basicAuth(function(user, pass){
+	return((user == 'cs360') && (pass == 'test'));
+});
+
+http.createServer(app).listen(80);
+https.createServer(options, app).listen(443);
+
+app.get('/', function(req, res){
+	res.send("Get Index");
+});
+
+app.use('/', express.static("./html", {maxAge: 60*60*1000}));
+
+app.get('/getcity', function(req, res){
+	fs.readFile('cities.dat.txt', function(err, data) {
+		if(err){
+			throw err;
+		}
+		var urlObj = url.parse(req.url, true, false);
+		var searchStr = urlObj.query.q;
+		var searchExp = new RegExp("^" + searchStr);
+                var returnValue = [];
+		var cities = data.toString().split("\n");
+		for(var i = 0; i < cities.length; i++){
+			var searchResult = cities[i].search(searchExp);
+			if(searchResult != -1){
+				returnValue.push({'city': cities[i]});
+			}
+		}
+		res.json(returnValue);
+	});
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+app.get('/comment', function(req, res){
+	var returnValue = "";
+	MongoClient.connect("mongodb://localhost/weather", function(err, db){
+		if(err){
+			throw err;
+		}
+		db.collection('comments', function(err, comments){
 			if(err){
 				throw err;
 			}
-			var searchStr = urlObj.query.q;
-			var searchExp = new RegExp("^" + searchStr);
-                        var returnValue = [];
-			var cities = data.toString().split("\n");
-			for(var i = 0; i < cities.length; i++){
-				var searchResult = cities[i].search(searchExp);
-				if(searchResult != -1){
-					returnValue.push({'city': cities[i]});
-				}
-			}
-			res.writeHead(200);
-			res.end(JSON.stringify(returnValue));
-		});
-	} else if(urlObj.pathname.indexOf("comment") != -1){
-		if(req.method === "POST"){
-			var jsonData = "";
-			req.on('data', function(chunk){
-				jsonData += chunk;
-			});
-			req.on('end', function(){
-				var reqObj = JSON.parse(jsonData);
-				console.log(reqObj);
-				MongoClient.connect("mongodb://localhost/weather", function(err, db){
-					if(err){
-						throw err;
-					}
-					db.collection('comments').insert(reqObj, function(err, records){
-						console.log("");
-					});	
+			comments.find(function(err, items){
+				items.toArray(function(err, itemArr){
+					res.json(itemArr);
 				});
 			});
-			res.writeHead(200);
-			res.end("");
-		} else if (req.method === "GET") {
-			var returnValue = "";
-			MongoClient.connect("mongodb://localhost/weather", function(err, db){
-				if(err){
-					throw err;
-				}
-				db.collection('comments', function(err, comments){
-					if(err){
-						throw err;
-					}
-					comments.find(function(err, items){
-						items.toArray(function(err, itemArr){
-							returnValue = JSON.stringify(itemArr);
-							res.writeHead(200);
-							res.end(returnValue);
-						});
-					});
-				});		
-			});
+		});		
+	});
+});
+
+app.post('/comment', auth, function(req, res){
+	MongoClient.connect("mongodb://localhost/weather", function(err, db){
+		if(err){
+			throw err;
 		}
-	} else {
-		fs.readFile(root_dir + urlObj.pathname, function(err, data){
-			if(err){
-				res.writeHead(404);
-				res.end(JSON.stringify(err));
-				return;
-			}
-			res.writeHead(200);
-			res.end(data);
-		});
-	}
-}).listen(80);	
+		db.collection('comments').insert(req.body, function(err, records){
+			console.log("");
+		});	
+	});
+	res.end();
+});
+
